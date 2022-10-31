b31f6afba3fc87324c7094273d6c8f3998004427
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2022-10-30 19:26:47 -0500

    Cleanup formatting

--- Grpc/Server/Program.cs
+++ Grpc/Server/Program.cs
@@ -17,25 +17,28 @@ var isProduction = builder.Environment.IsProduction();
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc(o =>
-{
-    o.EnableDetailedErrors = !isProduction;
-    // Wire up validation and exception handlers (note the sequence here is imperative)
-    // The interceptors are invoked in the sequence they are added since the exception
-    // Interceptor wraps the validation interceptor and maps the ValidationException
-    // to an RpcException it must be added first
-    o.Interceptors.Add<GrpcExceptionInterceptor>();
-    o.Interceptors.Add<GrpcValidationInterceptor>();
-});
-builder.Services
+_ = builder.Services
     .AddValidatorsFromAssemblyContaining<GreeterValidator>(includeInternalTypes: true)
+    .AddGrpc(o =>
+    {
+        o.EnableDetailedErrors = !isProduction;
+        // Wire up validation and exception handlers (note the sequence here is imperative)
+        // The interceptors are invoked in the sequence they are added since the exception
+        // Interceptor wraps the validation interceptor and maps the ValidationException
+        // to an RpcException it must be added first
+        o.Interceptors.Add<GrpcExceptionInterceptor>();
+        o.Interceptors.Add<GrpcValidationInterceptor>();
+    });
+_ = builder.Services
     .AddCodeFirstGrpc(o => o.EnableDetailedErrors = !isProduction);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
-app.MapGrpcService<WeatherForecastService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+_ = app.MapGrpcService<GreeterService>();
+_ = app.MapGrpcService<WeatherForecastService>();
+_ = app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 await app.RunAsync();
