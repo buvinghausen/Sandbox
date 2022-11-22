9c42b8ab05f02bcc15fea6355559881dc6705f4b
Brian Buvinghausen <buvinghausen@users.noreply.github.com>
2022-11-22 12:48:39 -0600

    Fix Guid & Time serialization issues

--- BlazorWasm/Server/Services/IdService.cs
+++ BlazorWasm/Server/Services/IdService.cs
@@ -1,11 +1,14 @@
 ﻿using BlazorWasm.Client.Services.Id;
 
+using Microsoft.AspNetCore.Authorization;
+
 using ProtoBuf.Grpc;
 
 using SequentialGuid;
 
 namespace BlazorWasm.Server.Services;
 
+[Authorize]
 internal sealed class IdService : IIdService
 {
     public ValueTask<IdResponse> GetIdAsync(CallContext context = default) =>
